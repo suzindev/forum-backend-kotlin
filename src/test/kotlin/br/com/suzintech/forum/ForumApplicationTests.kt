91279922b8b44a9a246c67543c9165013c2c6b3d
Fernando Suzin <suzin.dev@gmail.com>
2023-05-23 19:49:25 -0300

    Projeto criado

---
+++ src/test/kotlin/br/com/suzintech/forum/ForumApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.suzintech.forum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
